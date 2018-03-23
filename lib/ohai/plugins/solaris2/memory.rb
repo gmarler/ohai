2dfc21594acfcc07dca890f93d781f9489feaa8e
gmarler <gmarler@gmarler.com>
2018-03-23 13:21:08 -0400

    Comments on Virtual vs Physical Solaris Swap

--- lib/ohai/plugins/solaris2/memory.rb
+++ lib/ohai/plugins/solaris2/memory.rb
@@ -23,6 +23,23 @@ Ohai.plugin(:Memory) do
     meminfo = shell_out("prtconf | grep Memory").stdout
     memory[:total] = "#{meminfo.split[2].to_i * 1024}kB"
 
+    # There needs to be a distinction between Physical (Disk Backed) and 
+    # Virtual (RAM + Disk Backed) swap on Solaris.
+    # 'swap -s' deals entirely in Virtual Swap, leaving out all details regarding
+    # how much Physical Swap has been reserved against/used
+    # The crucial thing to understand about Solaris Virtual Swap is:
+    # - Physical (Disk Backed) swap is reserved against first, then RAM Backed Swap
+    #
+    # It is often necessary to determine the following info:
+    # - Total Virtual Swap (memory[:swap][:total])
+    # - Used Virtual Swap (memory[:swap][:total] - memory[:swap][:free])
+    # - Total Physical Swap (sum of blocks column in 'swap -l' output)
+    # - Used Physical Swap (Total Physical Swap - Used Virtual Swap)
+    #   - If Positive, you have Physical (Disk Backed) swap left
+    #   - If Negative, All Physical (Disk Backed) Swap has been used/
+    #     reserved against, and you're into RAM Backed Swap.
+    #     If you expect to have workload related memory shortfalls, you may
+    #     need to add more Physical Swap
     tokens = shell_out("swap -s").stdout.strip.split
     used_swap = tokens[8][0..-1].to_i #strip k from end
     free_swap = tokens[10][0..-1].to_i #strip k from end
